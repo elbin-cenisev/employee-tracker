35e4c3764f9b8cb071b1d5038fd2c3f0decbfe9b
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 19:21:24 -0400

    Populated department table for testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,6 @@
-INSERT INTO movies (movie_name)
-VALUES ("Cats"),
-       ("The Incredibles"),
-       ("Schindler's List");
-
-INSERT INTO reviews (review)
-VALUES ("Not enough mushrooms for this one"),
-       ("Pretty incredible"),
-       ("Made my dad cry");
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
        
