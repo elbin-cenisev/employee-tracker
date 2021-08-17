a9a29612eb7f4343eaab97c4ba7a30e6a4216e3d
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 18:57:30 -0400

    Updated with employee table; changed role to roles

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,21 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
