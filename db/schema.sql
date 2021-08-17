56178082aac0e4902fe54859f714e264ef2f0e1c
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 18:54:07 -0400

    Updated with role table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT NOT NULL
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
