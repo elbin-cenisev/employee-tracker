33f77aa8d44ab39ddb25439f5244fdf87eac1c9d
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 19:13:50 -0400

    Created main_menu function displaying choice

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the database.`)
 );
+
+async function main_menu() {
+  const mainMenuQs = [
+    {
+      name: 'selection',
+      type: 'list',
+      message: 'Who would you like to do?',
+      choices: ["View All Employees"]
+    }
+  ];
+  let selection = await inquirer.prompt(mainMenuQs);
+  console.log(`Hello World, you picked "` + selection.selection +`"`);
+}
+
+main_menu();
