37f0537d4e6bd506a0cc6ecd2d987d23b9f3cff4
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 19:18:49 -0400

    Added seeds file from previous activity

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO movies (movie_name)
+VALUES ("Cats"),
+       ("The Incredibles"),
+       ("Schindler's List");
+
+INSERT INTO reviews (review)
+VALUES ("Not enough mushrooms for this one"),
+       ("Pretty incredible"),
+       ("Made my dad cry");
+       
