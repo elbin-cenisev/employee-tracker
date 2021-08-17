c75e0746a10f3cbc4df0900586e85cfc2d31c397
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 19:07:57 -0400

    Fixed syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,7 @@ CREATE TABLE roles (
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT NOT NULL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -23,6 +22,6 @@ CREATE TABLE employee (
   last_name VARCHAR(30),
   role_id INT NOT NULL,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+  FOREIGN KEY (role_id) REFERENCES roles(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
