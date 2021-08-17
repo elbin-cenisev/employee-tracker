3623a56cd1d76392882b65c6496e1917d70778a4
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 18:48:08 -0400

    Basic server file added

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const DATABASE = ''
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+
+    // MySQL username,
+    user: 'root',
+
+    // MySQL password
+    password: 'mypass',
+    database: DATABASE
+  },
+  console.log(`Connected to the database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
