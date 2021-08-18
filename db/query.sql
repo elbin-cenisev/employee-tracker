610c134950b365c73ebf6620cc9529aa7c0a8760
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-17 20:03:37 -0400

    Changed department_name to name

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,7 @@
 SELECT 
     roles.id, 
     roles.title,
-    department.department_name AS department, 
+    department.name AS department, 
     roles.salary
 FROM roles
 JOIN department ON roles.department_id = department.id;
